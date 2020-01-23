b023954929a6c9059019a580f6dee9f9580f3363
Adam Deeley <adam.deeley@oneiota.co.uk>
2020-01-23 16:19:12 +0000

    Extract user router.

--- app.js
+++ app.js
@@ -3,35 +3,16 @@ const morgan = require('morgan')
 const connect = require('./connect')
 const { json, urlencoded } = require('body-parser')
 const app = express()
-const User = require('./models/user.model')
+const userRouter = require('./routes/users')
 
 app.use(morgan('dev'))
 app.use(urlencoded({ extended: true }))
 app.use(json())
+app.use('/', userRouter);
 
 app.get('/', (req, res) => {
 	res.send('OK')
 })
-
-app.post('/user', (req, res) => {
-	console.log(req.body);
-	const data = req.body;
-	User.create({
-		firstName: data.firstName,
-		lastName: data.lastName
-	})
-	res.send(req.body)
-})
-/*
-app.get('/user', (req, res) => {
-	console.log(req.body);
-	const data = req.body;
-	const result = User.find({
-		firstName: data.firstName,
-	})
-	console.log(result)
-	res.send(result)
-}) */
 
 connect('mongodb://localhost:27017/todo')
 	.then(() => app.listen(4000, () => {
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
+const User = require('../models/user.model')
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.route('/user')
+	.get(async (req, res) => {
+		const { firstName } = req.body;
+		const user = await User.find({
+			firstName
+		}).lean().exec()
+
+		res.status(200).json(user)
+	})
+	.post(async (req, res) => {
+		const { firstName, lastName } = req.body;
+		const user = await User.create({
+			firstName,
+			lastName
+		})
+
+		res.status(201).json(user.toJSON())
+	})
+
 
 module.exports = router;
